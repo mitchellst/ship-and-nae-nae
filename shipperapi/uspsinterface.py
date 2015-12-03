7bf631ffaff1eab2fbdd76ec58d14eab7288e522
Mitchell Stoutin <mitchell.stoutin@gmail.com>
2015-12-02 21:11:27 -0600

    step 1: get interview code working.

--- shipperapi/uspsinterface.py
+++ shipperapi/uspsinterface.py
@@ -2,6 +2,7 @@
 import math
 import requests
 import xml.etree.ElementTree as ET
+import html
 import os
 
 try:
@@ -9,9 +10,10 @@ try:
 except:
     raise RuntimeError("You don't have the Ordoro USPS API key loaded as an environment variable.")
 
-def get_rate_from_usps(zip_origin, zip_dest, ounces, width, height, depth):
+def get_rate_from_usps(zip_origin, zip_dest, ounces, width, height, depth, girth=None):
 
     size = 'REGULAR' if int(width)+int(height)+int(depth) < 12 else 'LARGE'
+    girth = int(width)*2 + int(depth)*2 if girth is None else girth
     ounces = float(ounces)
     pounds = math.floor(ounces/16)
     ozs = ounces % 16
@@ -26,9 +28,14 @@ def get_rate_from_usps(zip_origin, zip_dest, ounces, width, height, depth):
     <Ounces>{4}</Ounces>
     <Container />
     <Size>{5}</Size>
+    <Width>{6}</Width>
+    <Length>{7}</Length>
+    <Height>{8}</Height>
+    <Girth>{9}</Girth>
     <Machinable>true</Machinable>
     </Package>
-    </RateV4Request>""".format(ORDORO_USPS_KEY, zip_origin, zip_dest, pounds, ozs, size)
+    </RateV4Request>""".format(ORDORO_USPS_KEY, zip_origin, zip_dest, pounds, ozs,
+        size, width, height, depth, girth)
 
     return requests.get('http://production.shippingapis.com/ShippingAPI.dll?API=RateV4&XML='+rateV4Request)
 
@@ -38,7 +45,13 @@ def get_services_from_response(xmlstring):
     root = ET.fromstring(xmlstring)
     package = root.find('Package')
     for service in package.findall('Postage'):
-        out[service.find('MailService').text] = service.find('Rate').text
+        service_name_raw = service.find('MailService').text
+
+        #USPS returns weird superscript html characters, which it has to escape for XML.
+        # Make them unicode for our JSON response. Note: this leaves <sup> html tags in place.
+        service_name = html.unescape(service_name_raw.replace('&amp;', '&'))
+
+        out[service_name] = service.find('Rate').text
     return out
 
 
--- shipperapi/views.py
+++ shipperapi/views.py
@@ -7,8 +7,10 @@ def get_quote(request):
 
     getrate_args = [request.GET['or_zip'], request.GET['ds_zip'], request.GET['weight'],
         request.GET['dim_w'], request.GET['dim_h'], request.GET['dim_d']]
+    print(getrate_args)
     from_usps = get_rate_from_usps(*getrate_args)
+    print(from_usps.text)
     responseDict = get_services_from_response(from_usps.text)
-    response = HttpResponse(content_type="application/json")
+    response = HttpResponse(content_type="application/json", status=200)
     response.write(json.dumps(responseDict))
-    return response, 200
+    return response
